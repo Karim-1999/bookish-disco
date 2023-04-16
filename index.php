f1faa2d96c99ad0b8ad687a1e026c599e238291b
Zouhir Karim <105131699+Imparoprogrammazione99@users.noreply.github.com>
2023-04-16 14:30:37 +0200

    miglioramenti fatti1

--- index.php
+++ index.php
@@ -79,43 +79,39 @@ include('includes/functions.php');
 
 <!-- blog dynamic section -->
 
-
-<?php if ($stm = $connect->prepare('SELECT * FROM posts')) {
+<?php if ($stm = $connect->prepare('SELECT * FROM posts ORDER BY `date` DESC')) {
   $stm->execute();
 
   $result = $stm->get_result();
 
-
-
-
   if ($result->num_rows > 0) {
-
-
-   while ($record = mysqli_fetch_assoc($result)) {    ?>
-      <div class="container mt-5 pcpost">
-        <div class="row">
-          <?php while ($record = mysqli_fetch_assoc($result)) {?>
-            <div class="col-md-6 mb-4">
-              <div id="card" class="card  h-100">
-                <div class="card-body"><a  href="single.php?id=<?php echo $record['id']; ?>" >
+?>
+    <div class="container mt-5 pcpost">
+      <div class="row">
+        <?php while ($record = mysqli_fetch_assoc($result)) { ?>
+          <div class="col-md-6 mb-4">
+            <div id="card" class="card h-100">
+              <div class="card-body">
+                <a href="single.php?id=<?php echo $record['id']; ?>">
                   <img src="<?php echo $record['Url-image']; ?>" class="img-fluid mb-4 card-img-top rounded">
                   <h5 class="card-title"><?php echo $record['title']; ?></h5>
                   <p class="card-text"><?php echo substr(strip_tags($record['content']), 0, 230) . '...'; ?></p>
                   <p class="card-text"><?php /* echo $record['date']; */ ?></p>
-
-                 </a>
-                </div>
+                </a>
               </div>
             </div>
-          <?php } ?>
-        </div>
+          </div>
+        <?php } ?>
       </div>
-<?php }
+    </div>
+<?php
   }
 } ?>
 
+
+
 <!-- for mobile  -->
-<?php if ($stm = $connect->prepare('SELECT * FROM posts')) {
+<?php if ($stm = $connect->prepare('SELECT * FROM posts ORDER BY `date` DESC')) {
   $stm->execute();
 
   $result = $stm->get_result();
